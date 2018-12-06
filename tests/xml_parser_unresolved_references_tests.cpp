e810477faa9833bee1c26ab38008c67c9602ab67
Benjamin Weiss <benjamin.weiss@irt.de>
2018-12-06 16:24:54 +0100

    added unit tests for xml_parser_unresolved_references

---
+++ tests/xml_parser_unresolved_references_tests.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch.hpp>
+#include <sstream>
+#include "adm/parse.hpp"
+#include "adm/errors.hpp"
+
+std::string formatFilepath(const std::string& filename) {
+  std::stringstream ss;
+  ss << "xml_parser/unresolved_references/" << filename << ".xml";
+  return ss.str();
+}
+
+TEST_CASE("xml_parser/unresolved_references") {
+  for (auto filename :
+       {"audio_programme", "audio_content", "audio_object_1", "audio_object_2",
+        "audio_object_3", "audio_pack_format_1", "audio_pack_format_2",
+        "audio_track_uid_1", "audio_track_uid_2", "audio_track_format",
+        "audio_stream_format_1", "audio_stream_format_2"}) {
+    SECTION(filename) {
+      REQUIRE_THROWS_AS(adm::parseXml(formatFilepath(filename)),
+                        adm::error::XmlParsingUnresolvedReference);
+    }
+  }
+}
