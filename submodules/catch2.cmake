cf9b3890e4191ccaae945ebfccc6fe894eee9274
Michael Meier <meier@irt.de>
2018-12-10 19:21:51 +0100

    add cmake targets for included, 3rdparty, header-only libraries
    This provides more fine grained control about cmake settings/properties used for each target

---
+++ submodules/catch2.cmake
@@ -0,0 +1,3 @@
+add_library(catch2 STATIC ${PROJECT_SOURCE_DIR}/submodules/catch2/catch_main.cpp)
+target_include_directories(catch2 PUBLIC ${PROJECT_SOURCE_DIR}/submodules)
+target_compile_features(catch2 PUBLIC cxx_auto_type cxx_trailing_return_types)
---
+++ submodules/rapidxml.cmake
@@ -0,0 +1,2 @@
+add_library(rapidxml INTERFACE)
+target_include_directories(rapidxml INTERFACE ${PROJECT_SOURCE_DIR}/submodules)
