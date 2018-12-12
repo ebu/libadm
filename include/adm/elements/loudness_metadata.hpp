727c8718e5c1450a797e72476a3499d42098a2b5
Benjamin Weiss <benjamin.weiss@irt.de>
2018-12-12 18:45:34 +0100

    make the LoudnessMetadata class a proper libadm class

--- include/adm/elements/loudness_metadata.hpp
+++ include/adm/elements/loudness_metadata.hpp
@@ -1,31 +1,215 @@
 #pragma once
+#include "adm/detail/named_option_helper.hpp"
+#include "adm/detail/named_type.hpp"
+#include "adm/export.h"
+#include <boost/optional.hpp>
 #include <string>
 #include <ostream>
 
 namespace adm {
 
+  /// @brief Tag for NamedType ::LoudnessMethod
+  struct loudnessMethodTag {};
+  /// @brief NamedType for loudnessMethod parameter
+  using LoudnessMethod = detail::NamedType<std::string, loudnessMethodTag>;
+  /// @brief Tag for NamedType ::LoudnessRecType
+  struct loudnessRecTypeTag {};
+  /// @brief NamedType for loudnessRecType parameter
+  using LoudnessRecType = detail::NamedType<std::string, loudnessRecTypeTag>;
+  /// @brief Tag for NamedType ::LoudnessCorrectionType
+  struct loudnessCorrectionTypeTag {};
+  /// @brief NamedType for loudnessCorrectionType parameter
+  using LoudnessCorrectionType =
+      detail::NamedType<std::string, loudnessCorrectionTypeTag>;
+
+  /// @brief Tag for NamedType ::IntegratedLoudness
+  struct integratedLoudnessTag {};
+  /// @brief NamedType for integratedLoudness parameter
+  using IntegratedLoudness = detail::NamedType<float, integratedLoudnessTag>;
+  /// @brief Tag for NamedType ::LoudnessRange
+  struct loudnessRangeTag {};
+  /// @brief NamedType for loudnessRange parameter
+  using LoudnessRange = detail::NamedType<float, loudnessRangeTag>;
+  /// @brief Tag for NamedType ::MaxTruePeak
+  struct maxTruePeakTag {};
+  /// @brief NamedType for maxTruePeak parameter
+  using MaxTruePeak = detail::NamedType<float, maxTruePeakTag>;
+  /// @brief Tag for NamedType ::MaxMomentary
+  struct maxMomentaryTag {};
+  /// @brief NamedType for maxMomentary parameter
+  using MaxMomentary = detail::NamedType<float, maxMomentaryTag>;
+  /// @brief Tag for NamedType ::MaxShortTerm
+  struct maxShortTermTag {};
+  /// @brief NamedType for maxShortTerm parameter
+  using MaxShortTerm = detail::NamedType<float, maxShortTermTag>;
+  /// @brief Tag for NamedType ::DialogueLoudness
+  struct dialogueLoudnessTag {};
+  /// @brief NamedType for dialogueLoudness parameter
+  using DialogueLoudness = detail::NamedType<float, dialogueLoudnessTag>;
+
+  /// @brief Tag for LoudnessMetadata class
   struct LoudnessMetadataTag {};
 
   class LoudnessMetadata {
    public:
     typedef LoudnessMetadataTag tag;
 
-    LoudnessMetadata(){};
+    /**
+     * @brief Constructor template
+     *
+     * Templated constructor which accepts a variable number of ADM parameters
+     * in random order after the mandatory ADM parameters.
+     */
+    template <typename... Parameters>
+    LoudnessMetadata(Parameters... optionalNamedArgs);
 
-    std::string method;
-    std::string recType;
-    std::string correctionType;
+    /**
+     * @brief ADM parameter getter template
+     *
+     * Templated getter with the wanted ADM parameter type as template
+     * argument. If currently no value is available trying to get the adm
+     * parameter will result in an exception. Check with the has method before
+     */
+    template <typename Parameter>
+    Parameter get() const;
 
-    float integratedLoudness;
-    float loudnessRange;
-    float maxTruePeak;
-    float maxMomentary;
-    float maxShortTerm;
-    float dialogueLoudness;
+    /**
+     * @brief ADM parameter has template
+     *
+     * Templated has method with the ADM parameter type as template argument.
+     * Returns true if the ADM parameter is set or has a default value.
+     */
+    template <typename Parameter>
+    bool has() const;
+
+    /**
+     * @brief ADM parameter isDefault template
+     *
+     * Templated isDefault method with the ADM parameter type as template
+     * argument. Returns true if the ADM parameter is the default value.
+     */
+    template <typename Parameter>
+    bool isDefault() const;
+
+    /// @brief LoudnessMethod setter
+    ADM_EXPORT void set(LoudnessMethod loudnessMethod);
+    /// @brief LoudnessRecType setter
+    ADM_EXPORT void set(LoudnessRecType loudnessRecType);
+    /// @brief LoudnessCorrectionType setter
+    ADM_EXPORT void set(LoudnessCorrectionType loudnessLoudnessCorrectionType);
+    /// @brief IntegratedLoudness setter
+    ADM_EXPORT void set(IntegratedLoudness integratedLoudness);
+    /// @brief LoudnessRange setter
+    ADM_EXPORT void set(LoudnessRange loudnessRange);
+    /// @brief MaxTruePeak setter
+    ADM_EXPORT void set(MaxTruePeak maxTruePeak);
+    /// @brief MaxMomentary setter
+    ADM_EXPORT void set(MaxMomentary maxMomentary);
+    /// @brief MaxShortTerm setter
+    ADM_EXPORT void set(MaxShortTerm maxShortTerm);
+    /// @brief DialogueLoudness setter
+    ADM_EXPORT void set(DialogueLoudness dialogueLoudness);
+
+    /**
+     * @brief ADM parameter unset template
+     *
+     * Templated unset method with the ADM parameter type as template
+     * argument. Removes an ADM parameter if it is optional or resets it to
+     * the default value if there is one.
+     */
+    template <typename Parameter>
+    void unset();
 
     /**
      * @brief Print overview to ostream
      */
-    void print(std::ostream &os) const { os << "()"; };
+    void print(std::ostream &os) const;
+
+   private:
+    ADM_EXPORT LoudnessMethod
+        get(detail::ParameterTraits<LoudnessMethod>::tag) const;
+    ADM_EXPORT LoudnessRecType
+        get(detail::ParameterTraits<LoudnessRecType>::tag) const;
+    ADM_EXPORT LoudnessCorrectionType
+        get(detail::ParameterTraits<LoudnessCorrectionType>::tag) const;
+    ADM_EXPORT IntegratedLoudness
+        get(detail::ParameterTraits<IntegratedLoudness>::tag) const;
+    ADM_EXPORT LoudnessRange
+        get(detail::ParameterTraits<LoudnessRange>::tag) const;
+    ADM_EXPORT MaxTruePeak get(detail::ParameterTraits<MaxTruePeak>::tag) const;
+    ADM_EXPORT MaxMomentary
+        get(detail::ParameterTraits<MaxMomentary>::tag) const;
+    ADM_EXPORT MaxShortTerm
+        get(detail::ParameterTraits<MaxShortTerm>::tag) const;
+    ADM_EXPORT DialogueLoudness
+        get(detail::ParameterTraits<DialogueLoudness>::tag) const;
+
+    ADM_EXPORT bool has(detail::ParameterTraits<LoudnessMethod>::tag) const;
+    ADM_EXPORT bool has(detail::ParameterTraits<LoudnessRecType>::tag) const;
+    ADM_EXPORT bool has(
+        detail::ParameterTraits<LoudnessCorrectionType>::tag) const;
+    ADM_EXPORT bool has(detail::ParameterTraits<IntegratedLoudness>::tag) const;
+    ADM_EXPORT bool has(detail::ParameterTraits<LoudnessRange>::tag) const;
+    ADM_EXPORT bool has(detail::ParameterTraits<MaxTruePeak>::tag) const;
+    ADM_EXPORT bool has(detail::ParameterTraits<MaxMomentary>::tag) const;
+    ADM_EXPORT bool has(detail::ParameterTraits<MaxShortTerm>::tag) const;
+    ADM_EXPORT bool has(detail::ParameterTraits<DialogueLoudness>::tag) const;
+
+    template <typename Tag>
+    bool isDefault(Tag) const {
+      return false;
+    }
+
+    ADM_EXPORT void unset(detail::ParameterTraits<LoudnessMethod>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<LoudnessRecType>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<LoudnessCorrectionType>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<IntegratedLoudness>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<LoudnessRange>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<MaxTruePeak>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<MaxMomentary>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<MaxShortTerm>::tag);
+    ADM_EXPORT void unset(detail::ParameterTraits<DialogueLoudness>::tag);
+
+    boost::optional<LoudnessMethod> loudnessMethod_;
+    boost::optional<LoudnessRecType> loudnessRecType_;
+    boost::optional<LoudnessCorrectionType> loudnessCorrectionType_;
+    boost::optional<IntegratedLoudness> integratedLoudness_;
+    boost::optional<LoudnessRange> loudnessRange_;
+    boost::optional<MaxTruePeak> maxTruePeak_;
+    boost::optional<MaxMomentary> maxMomentary_;
+    boost::optional<MaxShortTerm> maxShortTerm_;
+    boost::optional<DialogueLoudness> dialogueLoudness_;
   };
+
+  // ---- Implementation ---- //
+
+  template <typename... Parameters>
+  LoudnessMetadata::LoudnessMetadata(Parameters... optionalNamedArgs) {
+    detail::setNamedOptionHelper(
+        this, std::forward<Parameters>(optionalNamedArgs)...);
+  };
+
+  template <typename Parameter>
+  Parameter LoudnessMetadata::get() const {
+    typedef typename detail::ParameterTraits<Parameter>::tag Tag;
+    return get(Tag());
+  }
+
+  template <typename Parameter>
+  bool LoudnessMetadata::has() const {
+    typedef typename detail::ParameterTraits<Parameter>::tag Tag;
+    return has(Tag());
+  }
+
+  template <typename Parameter>
+  bool LoudnessMetadata::isDefault() const {
+    typedef typename detail::ParameterTraits<Parameter>::tag Tag;
+    return isDefault(Tag());
+  }
+
+  template <typename Parameter>
+  void LoudnessMetadata::unset() {
+    typedef typename detail::ParameterTraits<Parameter>::tag Tag;
+    return unset(Tag());
+  }
 }  // namespace adm
---
+++ src/elements/loudness_metadata.cpp
@@ -0,0 +1,172 @@
+#include "adm/elements/loudness_metadata.hpp"
+
+#include <iomanip>
+
+namespace adm {
+
+  // ---- Getter ---- //
+  LoudnessMethod LoudnessMetadata::get(
+      detail::ParameterTraits<LoudnessMethod>::tag) const {
+    return loudnessMethod_.get();
+  }
+  LoudnessRecType LoudnessMetadata::get(
+      detail::ParameterTraits<LoudnessRecType>::tag) const {
+    return loudnessRecType_.get();
+  }
+  LoudnessCorrectionType LoudnessMetadata::get(
+      detail::ParameterTraits<LoudnessCorrectionType>::tag) const {
+    return loudnessCorrectionType_.get();
+  }
+  IntegratedLoudness LoudnessMetadata::get(
+      detail::ParameterTraits<IntegratedLoudness>::tag) const {
+    return integratedLoudness_.get();
+  }
+  LoudnessRange LoudnessMetadata::get(
+      detail::ParameterTraits<LoudnessRange>::tag) const {
+    return loudnessRange_.get();
+  }
+  MaxTruePeak LoudnessMetadata::get(
+      detail::ParameterTraits<MaxTruePeak>::tag) const {
+    return maxTruePeak_.get();
+  }
+  MaxMomentary LoudnessMetadata::get(
+      detail::ParameterTraits<MaxMomentary>::tag) const {
+    return maxMomentary_.get();
+  }
+  MaxShortTerm LoudnessMetadata::get(
+      detail::ParameterTraits<MaxShortTerm>::tag) const {
+    return maxShortTerm_.get();
+  }
+  DialogueLoudness LoudnessMetadata::get(
+      detail::ParameterTraits<DialogueLoudness>::tag) const {
+    return dialogueLoudness_.get();
+  }
+
+  // ---- Has ---- //
+  bool LoudnessMetadata::has(
+      detail::ParameterTraits<LoudnessMethod>::tag) const {
+    return loudnessMethod_ != boost::none;
+  }
+  bool LoudnessMetadata::has(
+      detail::ParameterTraits<LoudnessRecType>::tag) const {
+    return loudnessRecType_ != boost::none;
+  }
+  bool LoudnessMetadata::has(
+      detail::ParameterTraits<LoudnessCorrectionType>::tag) const {
+    return loudnessCorrectionType_ != boost::none;
+  }
+  bool LoudnessMetadata::has(
+      detail::ParameterTraits<IntegratedLoudness>::tag) const {
+    return integratedLoudness_ != boost::none;
+  }
+  bool LoudnessMetadata::has(
+      detail::ParameterTraits<LoudnessRange>::tag) const {
+    return loudnessRange_ != boost::none;
+  }
+  bool LoudnessMetadata::has(detail::ParameterTraits<MaxTruePeak>::tag) const {
+    return maxTruePeak_ != boost::none;
+  }
+  bool LoudnessMetadata::has(detail::ParameterTraits<MaxMomentary>::tag) const {
+    return maxMomentary_ != boost::none;
+  }
+  bool LoudnessMetadata::has(detail::ParameterTraits<MaxShortTerm>::tag) const {
+    return maxShortTerm_ != boost::none;
+  }
+  bool LoudnessMetadata::has(
+      detail::ParameterTraits<DialogueLoudness>::tag) const {
+    return dialogueLoudness_ != boost::none;
+  }
+
+  // ---- Setter ---- //
+  void LoudnessMetadata::set(LoudnessMethod loudnessMethod) {
+    loudnessMethod_ = loudnessMethod;
+  }
+  void LoudnessMetadata::set(LoudnessRecType loudnessRecType) {
+    loudnessRecType_ = loudnessRecType;
+  }
+  void LoudnessMetadata::set(LoudnessCorrectionType loudnessCorrectionType) {
+    loudnessCorrectionType_ = loudnessCorrectionType;
+  }
+  void LoudnessMetadata::set(IntegratedLoudness integratedLoudness) {
+    integratedLoudness_ = integratedLoudness;
+  }
+  void LoudnessMetadata::set(LoudnessRange loudnessRange) {
+    loudnessRange_ = loudnessRange;
+  }
+  void LoudnessMetadata::set(MaxTruePeak maxTruePeak) {
+    maxTruePeak_ = maxTruePeak;
+  }
+  void LoudnessMetadata::set(MaxMomentary maxMomentary) {
+    maxMomentary_ = maxMomentary;
+  }
+  void LoudnessMetadata::set(MaxShortTerm maxShortTerm) {
+    maxShortTerm_ = maxShortTerm;
+  }
+  void LoudnessMetadata::set(DialogueLoudness dialogueLoudness) {
+    dialogueLoudness_ = dialogueLoudness;
+  }
+
+  // ---- Unsetter ---- //
+  void LoudnessMetadata::unset(detail::ParameterTraits<LoudnessMethod>::tag) {
+    loudnessMethod_ = boost::none;
+  }
+  void LoudnessMetadata::unset(detail::ParameterTraits<LoudnessRecType>::tag) {
+    loudnessRecType_ = boost::none;
+  }
+  void LoudnessMetadata::unset(
+      detail::ParameterTraits<LoudnessCorrectionType>::tag) {
+    loudnessCorrectionType_ = boost::none;
+  }
+  void LoudnessMetadata::unset(
+      detail::ParameterTraits<IntegratedLoudness>::tag) {
+    integratedLoudness_ = boost::none;
+  }
+  void LoudnessMetadata::unset(detail::ParameterTraits<LoudnessRange>::tag) {
+    loudnessRange_ = boost::none;
+  }
+  void LoudnessMetadata::unset(detail::ParameterTraits<MaxTruePeak>::tag) {
+    maxTruePeak_ = boost::none;
+  }
+  void LoudnessMetadata::unset(detail::ParameterTraits<MaxMomentary>::tag) {
+    maxMomentary_ = boost::none;
+  }
+  void LoudnessMetadata::unset(detail::ParameterTraits<MaxShortTerm>::tag) {
+    maxShortTerm_ = boost::none;
+  }
+  void LoudnessMetadata::unset(detail::ParameterTraits<DialogueLoudness>::tag) {
+    dialogueLoudness_ = boost::none;
+  }
+
+  void LoudnessMetadata::print(std::ostream &os) const {
+    os << "(";
+    if (has<LoudnessMethod>()) {
+      os << "loudnessMethod=" << get<LoudnessMethod>() << ", ";
+    }
+    if (has<LoudnessRecType>()) {
+      os << "loudnessRecType=" << get<LoudnessRecType>() << ", ";
+    }
+    if (has<LoudnessCorrectionType>()) {
+      os << "loudnessCorrectionType=" << get<LoudnessCorrectionType>() << ", ";
+    }
+    if (has<IntegratedLoudness>()) {
+      os << "integratedLoudness=" << get<IntegratedLoudness>() << ", ";
+    }
+    if (has<LoudnessRange>()) {
+      os << "loudnessRange=" << get<LoudnessRange>() << ", ";
+    }
+    if (has<MaxTruePeak>()) {
+      os << "maxTruePeak=" << get<MaxTruePeak>() << ", ";
+    }
+    if (has<MaxMomentary>()) {
+      os << "maxMomentary=" << get<MaxMomentary>() << ", ";
+    }
+    if (has<MaxShortTerm>()) {
+      os << "maxShortTerm=" << get<MaxShortTerm>() << ", ";
+    }
+    if (has<DialogueLoudness>()) {
+      os << "dialogueLoudness=" << get<DialogueLoudness>() << ", ";
+    }
+    os << ")";
+  }
+
+}  // namespace adm
