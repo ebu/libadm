ecb447bf74a8b8b095f742c35bcd01836b01851f
Tom Nixon <tom@tomn.co.uk>
2019-08-14 11:22:29 +0100

    split hex tests and add a couple of extra cases

--- tests/hex_values_tests.cpp
+++ tests/hex_values_tests.cpp
@@ -1,14 +1,20 @@
 #include <catch2/catch.hpp>
 #include "adm/detail/hex_values.hpp"
 
-TEST_CASE("hex_values") {
-  using namespace adm;
+using namespace adm;
+
+TEST_CASE("parseHexValue") {
   REQUIRE(detail::parseHexValue("0000") == 0);
   REQUIRE(detail::parseHexValue("FFFF") == 65535);
   REQUIRE(detail::parseHexValue("0a0F") == 2575);
   REQUIRE(detail::parseHexValue("00ff") == 255);
   REQUIRE(detail::parseHexValue("ff") == 255);
   REQUIRE_THROWS_AS(detail::parseHexValue("ffff0"), std::runtime_error);
+  REQUIRE_THROWS_AS(detail::parseHexValue("fff "), std::runtime_error);
+  REQUIRE(detail::parseHexValue("ffff0", 8) == 1048560);
+}
+
+TEST_CASE("formatHexValue") {
   REQUIRE(detail::formatHexValue(255) == "00ff");
   REQUIRE(detail::formatHexValue(255, 2) == "ff");
   REQUIRE(detail::formatHexValue(65535, 8) == "0000ffff");
