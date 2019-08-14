e5052f47f25a3b3097436474083220773f59966b
Tom Nixon <tom@tomn.co.uk>
2019-08-14 11:21:07 +0100

    enable benchmarking feature in catch2

--- submodules/catch2.cmake
+++ submodules/catch2.cmake
@@ -1,3 +1,4 @@
 add_library(catch2 STATIC ${PROJECT_SOURCE_DIR}/submodules/catch2/catch_main.cpp)
 target_include_directories(catch2 PUBLIC ${PROJECT_SOURCE_DIR}/submodules)
 target_compile_features(catch2 PUBLIC cxx_auto_type cxx_trailing_return_types)
+target_compile_definitions(catch2 PUBLIC CATCH_CONFIG_ENABLE_BENCHMARKING)
