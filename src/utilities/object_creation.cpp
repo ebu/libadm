3e0b348da3e6f279d4c43a0de1e0f614ea7661d6
Benjamin Weiss <benjamin.weiss@irt.de>
2019-10-11 13:22:42 +0200

    added addChannelBedObjectTo function
    - ChannelBedObjectHolder struct added
    - to align API added addSimpleObjectTo function

--- src/utilities/object_creation.cpp
+++ src/utilities/object_creation.cpp
@@ -30,4 +30,36 @@ namespace adm {
 
     return holder;
   }
+
+  SimpleObjectHolder addSimpleObjectTo(std::shared_ptr<Document> document,
+                                       const std::string& name) {
+    auto holder = createSimpleObject(name);
+    document->add(holder.audioObject);
+    return holder;
+  }
+
+  ChannelBedObjectHolder addChannelBedObjectTo(
+      std::shared_ptr<Document> document, const std::string& name,
+      const std::string& speakerLayout) {
+    ChannelBedObjectHolder holder;
+    holder.audioObject = AudioObject::create(AudioObjectName(name));
+    auto packFormat =
+        document->lookup(audioPackFormatLookupTable().at(speakerLayout));
+    if (!packFormat) {
+      return holder;
+    }
+    holder.audioObject->addReference(packFormat);
+
+    auto labels = speakerLabelsLookupTable().at(speakerLayout);
+    for (auto label : labels) {
+      auto track = document->lookup(audioTrackFormatLookupTable().at(label));
+      auto uid = AudioTrackUid::create();
+      uid->setReference(packFormat);
+      uid->setReference(track);
+      holder.audioObject->addReference(uid);
+      holder.audioTrackUids[label] = uid;
+    }
+    document->add(holder.audioObject);
+    return holder;
+  }
 }  // namespace adm
