f8bab60d01c1f2007b29ebaa4122323bb5782e97
Tom Nixon <tom@tomn.co.uk>
2021-05-05 12:59:17 +0100

    add Gain class

---
+++ include/adm/elements/gain.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <cmath>
+
+namespace adm {
+  /// storage for gain parameters, which can either be a linear or dB value
+  class Gain {
+   public:
+    static Gain fromLinear(double linearGain) {
+      return Gain{linearGain, false};
+    }
+
+    static Gain fromDb(double dbGain) { return Gain{dbGain, true}; }
+
+    double asLinear() const {
+      if (isLinear())
+        return _gain;
+      else
+        return std::pow(10, _gain / 20.0);
+    }
+
+    double asDb() const {
+      if (isDb())
+        return _gain;
+      else
+        return 20.0 * std::log10(_gain);
+    }
+
+    bool isLinear() const { return !_isDb; }
+    bool isDb() const { return _isDb; }
+
+   private:
+    Gain(double gain, bool isDb) : _gain(gain), _isDb(isDb) {}
+
+    double _gain;
+    bool _isDb;
+  };
+}  // namespace adm
---
+++ tests/gain_tests.cpp
@@ -0,0 +1,31 @@
+#include <catch2/catch.hpp>
+#include <limits>
+#include <adm/elements/gain.hpp>
+
+using namespace adm;
+
+TEST_CASE("Gain linear") {
+  Gain g = Gain::fromLinear(10.0);
+  CHECK(g.isLinear());
+  CHECK(!g.isDb());
+  CHECK(g.asLinear() == 10.0);
+  CHECK(g.asDb() == Approx(20.0));
+}
+
+TEST_CASE("Gain linear 0") {
+  Gain g = Gain::fromLinear(0.0);
+  CHECK(g.asDb() == -std::numeric_limits<double>::infinity());
+}
+
+TEST_CASE("Gain Db") {
+  Gain g = Gain::fromDb(20.0);
+  CHECK(g.isDb());
+  CHECK(!g.isLinear());
+  CHECK(g.asDb() == 20.0);
+  CHECK(g.asLinear() == Approx(10.0));
+}
+
+TEST_CASE("Gain Db -inf") {
+  Gain g = Gain::fromDb(-std::numeric_limits<double>::infinity());
+  CHECK(g.asLinear() == 0.0);
+}
