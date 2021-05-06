ff6e5d1cc66b161e5fc32a85abf13ba804092c0a
Tom Nixon <tom@tomn.co.uk>
2021-05-06 11:56:04 +0100

    make functions in headers inline

--- include/adm/private/rapidxml_utils.hpp
+++ include/adm/private/rapidxml_utils.hpp
@@ -10,10 +10,10 @@ namespace adm {
       return std::count(begin, end, '\n');
     }
 
-    int getDocumentLine(rapidxml::xml_node<>* node) {
+    inline int getDocumentLine(rapidxml::xml_node<>* node) {
       return countLines(node->document()->first_node()->name(), node->name());
     }
-    int getDocumentLine(rapidxml::xml_attribute<>* attr) {
+    inline int getDocumentLine(rapidxml::xml_attribute<>* attr) {
       return countLines(attr->document()->first_node()->name(), attr->name());
     }
   }  // namespace xml
