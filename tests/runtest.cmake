4f7790ceca85f5e1a5eef9ddc5ef74c79719f8d8
Matt Firth <matt.firth@bbc.co.uk>
2021-05-17 14:05:45 +0100

    Only copy libs if ADM_SHARED_LIB is defined
    Cannot use BUILD_SHARED_LIBS as is not in scope of runtest.cmake

--- tests/runtest.cmake
+++ tests/runtest.cmake
@@ -1,11 +1,12 @@
-if(${BUILD_SHARED_LIBS})
-	execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${ADMLIB} ${TESTEXEDIR} RESULT_VARIABLE CMD_RESULT)
+if(DEFINED ADM_SHARED_LIB) # Do shared lib copy
+	message(STATUS "Shared lib copy: \n${ADM_SHARED_LIB} \n${TEST_EXE_DIR}")
+	execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${ADM_SHARED_LIB} ${TEST_EXE_DIR} RESULT_VARIABLE CMD_RESULT)
 	if(CMD_RESULT)
-		message(FATAL_ERROR "Error performing shared lib copy: \n${CMD_RESULT}: \n${RESULT_VARIABLE}:")
+		message(FATAL_ERROR "Error performing shared lib copy: \n${CMD_RESULT} \n${RESULT_VARIABLE}")
 	endif()
 endif()
 
-execute_process(COMMAND ${TESTEXE} RESULT_VARIABLE CMD_RESULT)
+execute_process(COMMAND ${TEST_EXE} RESULT_VARIABLE CMD_RESULT)
 if(CMD_RESULT)
-	message(FATAL_ERROR "Error running ${TESTEXE}: \n${CMD_RESULT}: \n${RESULT_VARIABLE}:")
+	message(FATAL_ERROR "Error running ${TEST_EXE}: \n${CMD_RESULT} \n${RESULT_VARIABLE}")
 endif()
