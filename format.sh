e7ada4363ed6c6647fa6acf8278ad614c2f142ac
Tom Nixon <tom@tomn.co.uk>
2021-05-21 18:43:29 +0100

    add format script

---
+++ format.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DIRS="examples include src tests tools"
+
+IGNORE="
+src/elements/time.cpp
+src/elements/audio_pack_format.cpp
+src/elements/audio_track_format_id.cpp
+src/elements/audio_block_format_id.cpp
+src/elements/audio_block_format_hoa.cpp
+src/document.cpp
+include/adm/elements/audio_block_format_hoa.hpp
+include/adm/elements/audio_pack_format.hpp
+include/adm/elements/audio_channel_format.hpp
+include/adm/elements/normalization.hpp
+include/adm/elements/audio_pack_format_hoa.hpp
+include/adm/elements/nfc_ref_dist.hpp
+tests/hex_values_tests.cpp
+tests/adm_common_definitions_tests.cpp
+tests/audio_pack_format_hoa_tests.cpp
+tests/xml_parser_audio_block_format_hoa_tests.cpp
+tests/xml_parser_audio_pack_format_tests.cpp
+"
+
+find $DIRS \( -iname '*.cpp' -or -iname '*.hpp' \) $(printf "! -wholename %s " $IGNORE) -exec clang-format -style=file -i '{}' +
