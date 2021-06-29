ad537a7884107c93f56fe2eac18605ee74469cc0
Richard Bailey <rsjtaylor.github@gmail.com>
2021-06-29 10:50:26 +0100

    Update AudioContent and AudioProgramme to use generic parameter checks

--- tests/audio_content_tests.cpp
+++ tests/audio_content_tests.cpp
@@ -2,9 +2,64 @@
 #include <catch2/catch.hpp>
 #include "adm/elements/audio_content.hpp"
 #include "adm/elements/audio_object.hpp"
+#include "helper/parameter_checks.hpp"
+#include "adm/utilities/element_io.hpp"
+
+using namespace adm;
+TEST_CASE("audio_content parameters") {
+  using namespace adm_test;
+  auto contentName = AudioContentName("MyContent");
+  auto audioContent = AudioContent::create(AudioContentName(contentName));
+  SECTION("AudioContentName") {
+    check_required_param<AudioContentName>(
+        audioContent, hasDefaultOf(contentName),
+        canBeSetTo(AudioContentName("SomethingElse")));
+  }
+  SECTION("AudioContentId") {
+    check_required_param<AudioContentId>(
+        audioContent, hasDefaultOf(AudioContentId{}),
+        canBeSetTo(AudioContentId(AudioContentIdValue(2))));
+  }
+  SECTION("AudioContentLanguage") {
+    check_optional_param<AudioContentLanguage>(
+        audioContent, canBeSetTo(AudioContentLanguage("de")));
+  }
+  SECTION("DialogId") {
+    check_optional_param<DialogueId>(audioContent,
+                                     canBeSetTo(Dialogue::NON_DIALOGUE));
+  }
+  SECTION("ContentKind") {
+    check_optional_param<ContentKind>(
+        audioContent, canBeSetTo(ContentKind{MixedContentKind{}}));
+  }
+  SECTION("ContentKind variants") {
+    SECTION("Dialog") {
+      check_optional_param<DialogueContentKind>(
+          audioContent, canBeSetTo(DialogueContent::COMMENTARY));
+    }
+    SECTION("NonDialog") {
+      check_optional_param<NonDialogueContentKind>(
+          audioContent, canBeSetTo(NonDialogueContent::EFFECT));
+    }
+    SECTION("Mixed") {
+      check_optional_param<MixedContentKind>(
+          audioContent, canBeSetTo(MixedContent::HEARING_IMPAIRED));
+    }
+  }
+  SECTION("LoudnessMetadatas") {
+    LoudnessMetadatas loudness = LoudnessMetadatas{
+        LoudnessMetadata{}, LoudnessMetadata{LoudnessMethod{"Guess"}}};
+    SECTION("check get/set") {
+      check_optional_param<LoudnessMetadatas>(audioContent,
+                                              canBeSetTo(loudness));
+    }
+    SECTION("add/remove") {
+      check_vector_param<LoudnessMetadatas>(audioContent, canBeSetTo(loudness));
+    }
+  }
+}
 
 TEST_CASE("audio_content") {
-  using namespace adm;
   {
     auto audioContent = AudioContent::create(AudioContentName("MyContent"));
     audioContent->set(AudioContentId(AudioContentIdValue(1)));
@@ -63,8 +118,6 @@ TEST_CASE("audio_content") {
 }
 
 TEST_CASE("audio_content_dialogue_interdependencies") {
-  using namespace adm;
-
   auto audioContent = AudioContent::create(AudioContentName("MyContent"));
 
   audioContent->set(DialogueContent::VOICEOVER);
