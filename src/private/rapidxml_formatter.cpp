60b3f9c8b0f00ac35b22c463bd2021ca49689a44
Tom Nixon <tom@tomn.co.uk>
2021-11-19 16:15:40 +0000

    use addVectorElements for loudnessMetadata

--- src/private/rapidxml_formatter.cpp
+++ src/private/rapidxml_formatter.cpp
@@ -56,7 +56,7 @@ namespace adm {
       node.addOptionalAttribute<End>(programme, "end");
       node.addOptionalAttribute<MaxDuckingDepth>(programme, "maxDuckingDepth");
       node.addReferences<AudioContent, AudioContentId>(programme, "audioContentIDRef");
-      node.addOptionalMultiElement<LoudnessMetadatas>(programme, "loudnessMetadata", &formatLoudnessMetadatas);
+      node.addVectorElements<LoudnessMetadatas>(programme, "loudnessMetadata", &formatLoudnessMetadata);
       // clang-format on
     }
 
@@ -79,14 +79,6 @@ namespace adm {
                                                 "dialogueLoudness");
     }
 
-    void formatLoudnessMetadatas(XmlNode &node, const std::string &name,
-                                 const LoudnessMetadatas &loudnessMetadatas) {
-      for (const auto &loudnessMetadata : loudnessMetadatas) {
-        auto loudnessMetadataNode = node.addNode(name);
-        formatLoudnessMetadata(loudnessMetadataNode, loudnessMetadata);
-      }
-    }
-
     void formatAudioContent(XmlNode &node,
                             std::shared_ptr<const AudioContent> content) {
       // clang-format off
@@ -94,7 +86,7 @@ namespace adm {
       node.addOptionalAttribute<AudioContentName>(content, "audioContentName");
       node.addOptionalAttribute<AudioContentLanguage>(content, "audioContentLanguage");
       node.addReferences<AudioObject, AudioObjectId>(content, "audioObjectIDRef");
-      node.addOptionalMultiElement<LoudnessMetadatas>(content, "loudnessMetadata", &formatLoudnessMetadatas);
+      node.addVectorElements<LoudnessMetadatas>(content, "loudnessMetadata", &formatLoudnessMetadata);
       node.addOptionalElement<NonDialogueContentKind>(content, "dialogue", &formatNonDialogueContentKind);
       node.addOptionalElement<DialogueContentKind>(content, "dialogue", &formatDialogueContentKind);
       node.addOptionalElement<MixedContentKind>(content, "dialogue", &formatMixedContentKind);
