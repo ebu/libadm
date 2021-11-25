b0e63d279dfa0dd63fdd9b79c6be15623bc748fc
Tom Nixon <tom@tomn.co.uk>
2021-11-25 17:06:25 +0000

    document Normalization default

--- include/adm/elements/normalization.hpp
+++ include/adm/elements/normalization.hpp
@@ -6,6 +6,6 @@
 namespace adm {
     /// @brief Tag for NamedType ::Normalization
     struct NormalizationTag {};
-    /// @brief NamedType for a normalization parameter
+    /// @brief NamedType for a normalization parameter, defaulting to SN3D
     using Normalization = detail::NamedType<std::string, NormalizationTag>;
 }  // namespace adm
