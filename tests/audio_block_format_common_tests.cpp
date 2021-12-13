03ca3ed038e034d893dafab910983f59527bdbc1
Tom Nixon <tom@tomn.co.uk>
2021-12-13 15:59:12 +0000

    simplify namespaces in audio_block_format_common_tests

--- tests/audio_block_format_common_tests.cpp
+++ tests/audio_block_format_common_tests.cpp
@@ -1,30 +1,30 @@
 #include "helper/parameter_checks.hpp"
 #include <adm/elements.hpp>
-namespace adm_test {
-  template <typename T>
-  struct BlockInitialiser {
-    static T create() { return T{}; }
-  };
-  template <>
-  struct BlockInitialiser<adm::AudioBlockFormatHoa> {
-    static adm::AudioBlockFormatHoa create() {
-      return adm::AudioBlockFormatHoa{adm::Order{}, adm::Degree{}};
-    }
-  };
-  template <>
-  struct BlockInitialiser<adm::AudioBlockFormatObjects> {
-    static adm::AudioBlockFormatObjects create() {
-      return adm::AudioBlockFormatObjects{adm::SphericalPosition{}};
-    }
-  };
-}  // namespace adm_test
+
+using namespace adm_test;
+using namespace adm;
+
+template <typename T>
+struct BlockInitialiser {
+  static T create() { return T{}; }
+};
+template <>
+struct BlockInitialiser<adm::AudioBlockFormatHoa> {
+  static adm::AudioBlockFormatHoa create() {
+    return adm::AudioBlockFormatHoa{adm::Order{}, adm::Degree{}};
+  }
+};
+template <>
+struct BlockInitialiser<adm::AudioBlockFormatObjects> {
+  static adm::AudioBlockFormatObjects create() {
+    return adm::AudioBlockFormatObjects{adm::SphericalPosition{}};
+  }
+};
 
 TEMPLATE_TEST_CASE("headphone parameter checks", "[parameter][template]",
                    adm::AudioBlockFormatObjects,
                    adm::AudioBlockFormatDirectSpeakers,
                    adm::AudioBlockFormatHoa) {
-  using namespace adm_test;
-  using namespace adm;
   auto blockFormat = BlockInitialiser<TestType>::create();
   // TODO Gain, Importance, HeadLocked
   SECTION("HeadphoneVirtualise") {
