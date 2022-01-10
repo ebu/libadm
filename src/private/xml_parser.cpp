325cb314519146897aa149b0b2f7c2980cb9c726
Tom Nixon <tom@tomn.co.uk>
2022-01-10 12:22:34 +0000

    xml parsing and generation for headLocked in audioBlockFormats

--- src/private/xml_parser.cpp
+++ src/private/xml_parser.cpp
@@ -431,6 +431,7 @@ namespace adm {
       setOptionalAttribute<Duration>(node, "duration", audioBlockFormat, &parseTimecode);
       setMultiElement<SpeakerPosition>(node, "position", audioBlockFormat, &parseSpeakerPosition);
       addOptionalElements<SpeakerLabel>(node, "speakerLabel", audioBlockFormat, &parseSpeakerLabel);
+      setOptionalElement<HeadLocked>(node, "headLocked", audioBlockFormat);
       setOptionalElement<HeadphoneVirtualise>(node, "headphoneVirtualise", audioBlockFormat, &parseHeadphoneVirtualise);
       // clang-format on
       setOptionalElement<Gain>(node, "gain", audioBlockFormat, &parseGain);
@@ -618,6 +619,7 @@ namespace adm {
       setOptionalElement<JumpPosition>(node, "jumpPosition", audioBlockFormat, &parseJumpPosition);
       setOptionalElement<ScreenRef>(node, "screenRef", audioBlockFormat);
       setOptionalElement<Importance>(node, "importance", audioBlockFormat);
+      setOptionalElement<HeadLocked>(node, "headLocked", audioBlockFormat);
       setOptionalElement<HeadphoneVirtualise>(node, "headphoneVirtualise", audioBlockFormat, &parseHeadphoneVirtualise);
       // clang-format on
       return audioBlockFormat;
@@ -837,6 +839,7 @@ namespace adm {
       setOptionalElement<ScreenRef>(node, "screenRef", audioBlockFormat);
       setOptionalElement<Normalization>(node, "normalization", audioBlockFormat);
       setOptionalElement<Equation>(node, "equation", audioBlockFormat);
+      setOptionalElement<HeadLocked>(node, "headLocked", audioBlockFormat);
       setOptionalElement<HeadphoneVirtualise>(node, "headphoneVirtualise", audioBlockFormat, &parseHeadphoneVirtualise);
       setOptionalElement<Gain>(node, "gain", audioBlockFormat, &parseGain);
       setOptionalElement<Importance>(node, "importance", audioBlockFormat);
