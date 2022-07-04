8657bacc33a208b2a724bc044956b599e584b060
Thomas Nixon <tom@tomn.co.uk>
2022-07-04 16:16:59 +0100

    tidy HOA audioPackFormat tests
    remove redundant tests, and use new helpers

--- tests/audio_pack_format_hoa_tests.cpp
+++ tests/audio_pack_format_hoa_tests.cpp
@@ -1,89 +1,34 @@
 #define CATCH_CONFIG_ENABLE_CHRONO_STRINGMAKER
 #include <catch2/catch.hpp>
-#include "adm/elements/audio_channel_format.hpp"
 #include "adm/elements/audio_pack_format_hoa.hpp"
+#include "helper/parameter_checks.hpp"
 
-TEST_CASE("audio_pack_format_hoa_without_extra_param") {
+TEST_CASE("audio_pack_format_hoa") {
   using namespace adm;
-
+  using namespace adm_test;
   auto packFormatHoa =
       AudioPackFormatHoa::create(AudioPackFormatName("MyPackFormatHoa"));
-  packFormatHoa->set(
-      AudioPackFormatId(TypeDefinition::HOA, AudioPackFormatIdValue(1)));
-  packFormatHoa->set(AudioPackFormatName("MyNewPackFormatHoa"));
-  packFormatHoa->set(Importance(5));
-  packFormatHoa->set(AbsoluteDistance(4.5));
 
-  REQUIRE(packFormatHoa->has<AudioPackFormatId>());
-  REQUIRE(packFormatHoa->has<AudioPackFormatName>());
-  REQUIRE(packFormatHoa->has<TypeDescriptor>());
-  REQUIRE(packFormatHoa->has<Importance>());
-  REQUIRE(packFormatHoa->has<AbsoluteDistance>());
+  check_required_param<AudioPackFormatName>(
+      packFormatHoa, hasDefaultOf(AudioPackFormatName("MyPackFormatHoa")),
+      canBeSetTo(AudioPackFormatName("MyPackFormatHoaTest")));
 
-  REQUIRE(packFormatHoa->get<AudioPackFormatId>().get<TypeDescriptor>() ==
-          TypeDefinition::HOA);
-  REQUIRE(
-      packFormatHoa->get<AudioPackFormatId>().get<AudioPackFormatIdValue>() ==
-      1u);
-  REQUIRE(packFormatHoa->get<AudioPackFormatName>() == "MyNewPackFormatHoa");
-  REQUIRE(packFormatHoa->get<TypeDescriptor>() == TypeDefinition::HOA);
-  REQUIRE(packFormatHoa->get<Importance>() == 5);
-  REQUIRE(packFormatHoa->get<AbsoluteDistance>() == 4.5);
+  // no default is specified for audioPackFormat for some reason
+  check_optional_param<Importance>(packFormatHoa, canBeSetTo(Importance(5)));
 
-  packFormatHoa->unset<Importance>();
-  packFormatHoa->unset<AbsoluteDistance>();
+  check_optional_param<AbsoluteDistance>(packFormatHoa,
+                                         canBeSetTo(AbsoluteDistance(2.0)));
 
-  REQUIRE(!packFormatHoa->has<Importance>());
-  REQUIRE(!packFormatHoa->has<AbsoluteDistance>());
-}
+  check_defaulted_param<Normalization>(packFormatHoa,
+                                       hasDefaultOf(Normalization("SN3D")),
+                                       canBeSetTo(Normalization("N3D")));
 
-TEST_CASE("audio_pack_format_hoa_with_all_extra_param") {
-    using namespace adm;
+  check_defaulted_param<NfcRefDist>(packFormatHoa,
+                                    hasDefaultOf(NfcRefDist(1.0)),
+                                    canBeSetTo(NfcRefDist(0.0)));
 
-    auto packFormatHoa =
-        AudioPackFormatHoa::create(AudioPackFormatName("MyPackFormatHoa"));
-    packFormatHoa->set(AudioPackFormatId(TypeDefinition::HOA,
-                                         AudioPackFormatIdValue(1)));
-    packFormatHoa->set(AudioPackFormatName("MyNewPackFormatHoa"));
-    packFormatHoa->set(Normalization("N3D"));
-    packFormatHoa->set(ScreenRef(true));
-    packFormatHoa->set(NfcRefDist(2));
-
-    REQUIRE(packFormatHoa->has<AudioPackFormatId>());
-    REQUIRE(packFormatHoa->has<AudioPackFormatName>());
-    REQUIRE(packFormatHoa->has<TypeDescriptor>());
-    REQUIRE(packFormatHoa->has<Normalization>());
-    REQUIRE(packFormatHoa->has<ScreenRef>());
-    REQUIRE(packFormatHoa->has<NfcRefDist>());
-
-    REQUIRE(packFormatHoa->get<AudioPackFormatId>().get<TypeDescriptor>() ==
-            TypeDefinition::HOA);
-    REQUIRE(
-        packFormatHoa->get<AudioPackFormatId>().get<AudioPackFormatIdValue>() ==
-        1u);
-    REQUIRE(packFormatHoa->get<AudioPackFormatName>() == "MyNewPackFormatHoa");
-    REQUIRE(packFormatHoa->get<TypeDescriptor>() ==
-            TypeDefinition::HOA);
-    REQUIRE(packFormatHoa->get<Normalization>() == "N3D");
-    REQUIRE(packFormatHoa->get<ScreenRef>() == true);
-    REQUIRE(packFormatHoa->get<NfcRefDist>() == 2);
-
-    REQUIRE(!packFormatHoa->isDefault<Normalization>());
-    REQUIRE(!packFormatHoa->isDefault<ScreenRef>());
-    REQUIRE(!packFormatHoa->isDefault<NfcRefDist>());
-
-    packFormatHoa->unset<Normalization>();
-    packFormatHoa->unset<ScreenRef>();
-    packFormatHoa->unset<NfcRefDist>();
-
-    REQUIRE(packFormatHoa->isDefault<Normalization>());
-    REQUIRE(packFormatHoa->isDefault<ScreenRef>());
-    REQUIRE(packFormatHoa->isDefault<NfcRefDist>());
-
-    REQUIRE(packFormatHoa->get<Normalization>() == "SN3D");
-    REQUIRE(packFormatHoa->get<ScreenRef>() == false);
-    REQUIRE(packFormatHoa->get<NfcRefDist>() == 0);
-
+  check_defaulted_param<ScreenRef>(packFormatHoa, hasDefaultOf(ScreenRef(true)),
+                                   canBeSetTo(ScreenRef(false)));
 }
 
 TEST_CASE("audio_pack_format_hoa_with_extra_param_in_constructor") {
